23ecfec765b8a67c71103edb7f11ee36daf54d69
Adamu680 <118018871+Adamu680@users.noreply.github.com>
2022-12-22 09:20:42 +0100

    Create challege.js

---
+++ academychallenges/module_02/Grade Calculator/challege.js
@@ -0,0 +1,17 @@
+let percentage = (totalgrades / 400) * 100; 
+
+  if (percentage <= 100 && percentage >= 80) { 
+
+    grades = "A"; 
+
+  } else if (percentage <= 79 && percentage >= 60) { 
+
+    grades = "B"; 
+
+  } else if (percentage <= 59 && percentage >= 40) { 
+
+    grades = "C"; 
+
+  } else { 
+
+    grades = "F"; 
